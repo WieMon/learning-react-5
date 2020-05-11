6d577bb899746a395c0a3040132db2a3ab67902b
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-11 18:53:37 +0200

    Finish initializing project Add the components: AddTask, TaskList and Task

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        TODO APP
+        <AddTask />
+        <TaskList />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './Task';
+
+const TaskList = () => {
+  return ( 
+    <div>
+      <h2>Task list</h2>
+      <Task />
+      <Task />
+    </div>
+   );
+}
+ 
+export default TaskList;
