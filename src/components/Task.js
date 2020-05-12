c5e4faf9fe8551fa19bcc54b1fdbfdb77e73563d
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-12 12:24:21 +0200

    Add prioritizing tasks and date of completion

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
 const Task = (props) => {
-  const {text, date, id} = props.task;
-  return ( 
-    <div>
+
+   const style = {
+    color: 'red',
+  }
+  const {text, date, id, active, important, finishDate} = props.task;
+ 
+  if(active) {
+    return ( 
+      <div>
+        <p>
+          <strong style={important ? style : null}>{text}</strong> - <span>{date} </span>
+          <button onClick={() => props.change(id)}>Done</button>
+          <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </div>
+    );
+  } else {
+    const finish = new Date(finishDate).toLocaleString()
+   return (
+     <div>
       <p>
-        <strong>{text}</strong> - <span>{date} </span>
-        <button onClick={() => props.change(id)}>Done</button>
+        <strong>{text}</strong><em> (deadline: {date})</em>
+        <br />
+        - completed<span> {finish} </span>
         <button onClick={() => props.delete(id)}>X</button>
       </p>
-    </div>
-   );
+   </div>
+   )
+  }
 }
  
 export default Task;
